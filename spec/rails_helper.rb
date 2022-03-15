bfee40981d2c1adc1686fd6bd5f58be4f31af490
Fotima Khamroeva <bonu_9229@bk.ru>
2022-03-15 01:50:19 -0400

    added spec test for home page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,7 +17,7 @@ require "capybara/poltergeist" # Add this line to require poltergeist
 # that JS driver to :poltergeist
 Capybara.javascript_driver = :poltergeist
 
-Copybara.default_max_wait_time = 10
+#Copybara.default_max_wait_time = 10
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
